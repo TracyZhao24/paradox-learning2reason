bdea0d846aa4a7d0b71ba19dc2bf5d941f7ef94f
Tracy Zhao <tracyzhao24@g.ucla.edu>
2024-03-02 16:47:23 -0800

    use test dataset

--- scripts/11_eval_trained_logic_bert.bash
+++ scripts/11_eval_trained_logic_bert.bash
@@ -1,6 +1,6 @@
 python logic_bert/eval_trained_logic_bert.py \
-                --data_file DATA/RP/prop_examples.balanced_by_backward.max_6.json \
+                --data_file DATA/LP/prop_examples.balanced_by_backward.max_6.json \
                 --device cuda --cuda_core 1 \
-                --batch_size 16 \
-                --log_file eval_rp.txt \
+                --batch_size 1 \
+                --log_file logs/eval_logic_bert_lp.txt \
                 --vocab_file sample/vocab.txt
--- scripts/9_eval_logic_bert.bash
+++ scripts/9_eval_logic_bert.bash
@@ -1,4 +1,5 @@
 python logic_bert/evaluate.py \
-	--data_file DATA/LP/prop_examples.balanced_by_backward.max_6.json_val \
-	--log_file eval_lp.txt \
+	--data_file DATA/LP/prop_examples.balanced_by_backward.max_6.json \
+	--log_file logs/eval_lp.txt \
+	--device cuda --cuda_core 1 \
 	--vocab_file sample/vocab.txt
